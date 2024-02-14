549c3cbe1b32a61aece0546296118372ad90ef5d
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-15 01:16:47 +0530

    Auto Connect to Room Feature

--- events/join_room_handler.js
+++ events/join_room_handler.js
@@ -1,26 +1,33 @@
 const Room = require("../room");
+const crypto = require("crypto");
 
 module.exports = (socket, io, rooms) => {
-    return ({ roomId, sender, player}) => {
-    const room = rooms[roomId] || new Room(roomId);
-
-    if (!room.isFull()) {
-      room.addPlayer({ id: sender, symbol: player});
-      socket.join(roomId);
-      rooms[roomId] = room;
-
-      if (room.players.length === 1) {
-        io.to(roomId).emit("waitingForOpponent");
-      } else {
-        io.to(roomId).emit("gameStart", true);
-        io.to(roomId).emit("updateBoard", room.gameBoard);
-        const randomPlayer = room.players[Math.floor(Math.random() * 2)];
-        io.to(roomId).emit("handleTurns", randomPlayer.symbol);
-      }
+  return (data) => {
+    let room;
+    const availableRoom = Object.values(rooms).find((room) => !room.isFull());
+    if (availableRoom) {
+      room = availableRoom;
+      room.addPlayer({ id: data.sender, symbol: data.player });
+      socket.join(room.roomId);
+      io.to(room.roomId).emit("joinRoomResponse", {...data, roomId: room.roomId})
     } else {
-      socket.emit("roomFull");
+      const roomId = crypto.randomBytes(8).toString("hex");
+      room = new Room(roomId);
+      room.addPlayer({ id: data.sender, symbol: data.player });
+      socket.join(room.roomId);
+      rooms[roomId] = room;
+      io.to(room.roomId).emit("joinRoomResponse", {...data, roomId: room.roomId})
     }
 
-    console.log(`${player} joined the room: ${roomId}`);
-  }
-}
+    if (room.players.length === 1) {
+      io.to(room.roomId).emit("waitingForOpponent");
+    } else {
+      io.to(room.roomId).emit("gameStart", true);
+      io.to(room.roomId).emit("updateBoard", room.gameBoard);
+      const randomPlayer = room.players[Math.floor(Math.random() * 2)];
+      io.to(room.roomId).emit("handleTurns", randomPlayer.symbol);
+    }
+    console.log(room)
+    console.log(`${data.player} joined the room: ${room.roomId}`);
+  };
+};
