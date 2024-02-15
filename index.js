803467c4070cdac767fc8169e2aa75b231cc9067
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-15 20:22:47 +0530

    Fixed Cors URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
@@ -17,7 +17,12 @@ const PORT = process.env.PORT || 4000;
 
 const rooms = {};
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST"],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Tic Tac Toe Server is running!");
@@ -26,8 +31,14 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("User Connected: ", socket.id);
 
-  socket.on("join_room", require("./events/join_room_handler")(socket, io, rooms));
-  socket.on("leave_room", require("./events/leave_room_handler")(socket, io, rooms));
+  socket.on(
+    "join_room",
+    require("./events/join_room_handler")(socket, io, rooms)
+  );
+  socket.on(
+    "leave_room",
+    require("./events/leave_room_handler")(socket, io, rooms)
+  );
   socket.on("move", require("./events/move_handler")(io, rooms));
   socket.on("disconnect", () => {
     console.log("User disconnected");
