f0f170ffb63ca8e05ddc1e03cb093029ffadcef8
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-16 13:07:52 +0530

    Link Updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://tic-tac-toe-next-omega.vercel.app",
+    origin: process.env.CORS_ORIGIN,
     methods: ["GET", "POST"],
   },
 });
