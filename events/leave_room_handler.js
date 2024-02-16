be7c0288582eb24558c1d37624f7f0529d077736
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-16 21:24:18 +0530

    Optimized Leave Room Logic

--- events/leave_room_handler.js
+++ events/leave_room_handler.js
@@ -4,12 +4,14 @@ module.exports = (socket, io, rooms) => {
       rooms[roomId].players = rooms[roomId].players.filter(
         (p) => p.id !== socket.id
       );
+      rooms[roomId].turn = '';
       socket.leave(roomId);
 
       if (rooms[roomId].players.length === 0) {
         delete rooms[roomId];
       } else {
         io.to(roomId).emit("opponentLeft");
+        rooms[roomId].resetGame();
       }
 
       console.log(`${socket.id} left the room: ${roomId}`);
