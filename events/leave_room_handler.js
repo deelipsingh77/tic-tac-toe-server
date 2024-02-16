c47c779b1c9c9b1d2a99c5046f6bf70f4c7707f4
Deelip Kumar Singh <deelipsingh77@gmail.com>
2024-02-16 22:51:39 +0530

    Leave Room Logic Fixed

--- events/leave_room_handler.js
+++ events/leave_room_handler.js
@@ -1,20 +1,16 @@
 module.exports = (socket, io, rooms) => {
-    return ({ roomId }) => {
+  return ({ roomId }) => {
     if (rooms[roomId]) {
       rooms[roomId].players = rooms[roomId].players.filter(
         (p) => p.id !== socket.id
       );
-      rooms[roomId].turn = '';
+      rooms[roomId].turn = "";
       socket.leave(roomId);
 
-      if (rooms[roomId].players.length === 0) {
-        delete rooms[roomId];
-      } else {
-        io.to(roomId).emit("opponentLeft");
-        rooms[roomId].resetGame();
-      }
+      delete rooms[roomId];
+      io.to(roomId).emit("opponentLeft");
 
       console.log(`${socket.id} left the room: ${roomId}`);
     }
-  }
-}
+  };
+};
